e6e412e8543294748a87246460a8b392b278e7a6
arturianec100 <arturianec100@gmail.com>
2021-02-08 20:13:40 +0200

    hwparser advanced

--- src/hwparser.cpp
+++ src/hwparser.cpp
@@ -8,6 +8,33 @@ HWParser::HWParser(const char *first_, const char *last_):
 ParseResult HWParser::parse()
 {
     ParseResult result;
+    QTextStream out(&result.output);
+    ctx = {};
+    ctx.resPtr = &result;
+    ctx.outPtr = &out;
+
+    while (ctx.shouldContinue) {
+        if (isEnd()) {
+            break;//loop
+        }
+        skip();
+        switch (ctx.stage) {
+        case Context::NoTable:
+            if ((!isSpecialState()) && (peek(4) == "char")) {
+                ctx.stage = Context::Type;
+                moveBy(4);
+                break;//switch
+            }
+            skipTo(';');
+            ++current;
+            break;
+        case Context::Type:
+            //
+            break;
+        default:
+            ctx.shouldContinue = false;
+        }
+    }
 
     return result;
 }
@@ -24,43 +51,125 @@ std::string_view HWParser::consume(std::size_t count)
     return str;
 }
 
+void HWParser::step()
+{
+    ++current;
+    while ((!isEnd()) && (isOnSingleQuote() || isOnDoubleQuote())) {
+        if (isOnSingleQuote() && (!isOnDoubleQuote())) {
+            ctx.isSingleQuotes = !ctx.isSingleQuotes;
+            ++current;
+        }
+        if (isOnDoubleQuote() && (!isOnSingleQuote())) {
+            ctx.isDoubleQuotes = !ctx.isDoubleQuotes;
+            ++current;
+        }
+    }
+}
+
+void HWParser::moveBy(std::size_t chars)
+{
+    current += chars;
+}
+
+std::size_t HWParser::pos() const
+{
+    return static_cast<size_t>(current - first);
+}
+
+bool HWParser::isSpecialState() const
+{
+    return ctx.isOneLineComment || ctx.isMultiLineComment
+            || ctx.isSingleQuotes || ctx.isDoubleQuotes;
+}
+
 bool HWParser::isSpace() const
 {
     return std::isspace(static_cast<unsigned char>(*current))
             || std::iscntrl(static_cast<unsigned char>(*current));
 }
 
+bool HWParser::isEnd()
+{
+    return current >= last ? !(ctx.shouldContinue = false) : false;
+}
+
+bool HWParser::isOnSingleQuote() const
+{
+    return ((*current) == '\'') && ((*(current - 1)) != '\\');
+}
+
+bool HWParser::isOnDoubleQuote() const
+{
+    return ((*current) == '"') && ((*(current - 1)) != '\\');
+}
+
 bool HWParser::shouldSkip()
 {
-    return ctx.isOneLineComment || ctx.isMultiLineComment
+    return (!isEnd()) && (ctx.isOneLineComment || ctx.isMultiLineComment
             || (ctx.isOneLineComment = (peek(2) == "//"))
             || (ctx.isMultiLineComment = (peek(2) == "/*"))
-            || isSpace();
+            || isSpace());
 }
 
 void HWParser::skip()
 {
-    //inside shouldSkip() ctx.is... are updated
+    //inside shouldSkip() ctx.is...Comment are updated
     while (shouldSkip()) {
 
-        while(isSpace()) {
+        while(isSpace() && (!isEnd())) {
             ++current;
         }
 
         if (ctx.isMultiLineComment) {
-            while (peek(2) != "*/") {
+            while ((peek(2) != "*/") && (!isEnd())) {
                 ++current;
             }
-            current += 2;//don't point to "*/"
+            if (!isEnd()) {
+                current += 2;//don't point to "*/"
+            }
             ctx.isMultiLineComment = false;
         }
 
         if (ctx.isOneLineComment) {
-            while ((*current) != '\n') {
+            while (((*current) != '\n') && (!isEnd())) {
                 ++current;
             }
-            ++current;//don't point to '\n'
+            if (!isEnd()) {
+                ++current;//don't point to '\n'
+            }
             ctx.isOneLineComment = false;
+        }
+    }
+}
+
+void HWParser::skipTo(char c)
+{
+    //while (a && (b || c))
+    while ((!isEnd()) && (
+               ((*current) != c)
+                || (ctx.isSingleQuotes
+                    || ctx.isDoubleQuotes)
+               )
+           ) {
+        if (ctx.isSingleQuotes
+                || ctx.isDoubleQuotes) {
+            skipToEndOfQuotes();
+        }
+        skip();
+        step();
+    }
+}
+
+void HWParser::skipToEndOfQuotes()
+{
+    if (ctx.isSingleQuotes) {
+        while ((!isEnd()) && ctx.isSingleQuotes) {
+            step();
+        }
+    }
+    if (ctx.isDoubleQuotes) {
+        while ((!isEnd()) && ctx.isDoubleQuotes) {
+            step();
         }
     }
 }
--- src/hwparser.h
+++ src/hwparser.h
@@ -11,18 +11,30 @@ public:
     using namespace std;
 
     HWParser(const char* first_, const char* last_);
-
+    //Api
     ParseResult parse();
 
 protected:
-    //Api
-    string_view peek(size_t count);
-    string_view consume(size_t count);
+    //Inner api
+    inline string_view peek(size_t count);
+    inline string_view consume(size_t count);
+    inline void step();
+    inline void moveBy(size_t chars);
 
-    bool isSpace() const;
+    inline size_t pos() const;
 
-    bool shouldSkip();
-    void skip();
+    inline bool isSpecialState() const;
+
+    inline bool isSpace() const;
+    inline bool isEnd();
+
+    inline bool isOnSingleQuote() const;
+    inline bool isOnDoubleQuote() const;
+
+    inline bool shouldSkip();
+    inline void skip();
+    inline void skipTo(char c);
+    inline void skipToEndOfQuotes();
     //Data
     const char* first;
     const char* last;
@@ -30,8 +42,13 @@ protected:
     Context ctx;
     //Types
     struct Context {
+        bool shouldContinue = true;
         bool isOneLineComment = false;
         bool isMultiLineComment = false;
+        bool isSingleQuotes = false;
+        bool isDoubleQuotes = false;
+        ParseResult *resPtr = nullptr;
+        QTextStream *outPtr = nullptr;
         TableStage stage = NoTable;
         enum TableStage { NoTable = -1, Type, Identifier, Sizing, Column, Row };
     };
